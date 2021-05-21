a118e5b922ca22cff8651731cdb4cebd1faabcff
정물결 <jmk1473@naver.com>
2021-05-21 13:22:53 +0900

    feat : Add Navbar

---
+++ happy-house-front/src/components/header/BasicHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <baisc-navbar></baisc-navbar>
+        sdf
+    </div>
+</template>
+
+<script>
+import BaiscNavbar from "../navbar/BasicNavbar.vue";
+
+
+export default {
+    name: "BaiscHeader",
+    components: {
+        BaiscNavbar,
+
+    },
+}
+</script>
+
+<style>
+
+</style>
