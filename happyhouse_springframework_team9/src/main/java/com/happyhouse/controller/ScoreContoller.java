4cdb22688b06ddef5eaabeb048437f066a216c3d
임성애 <tjddo2791@naver.com>
2021-05-23 02:35:50 +0900

    create : ScoreController 설계

---
+++ happyhouse_springframework_team9/src/main/java/com/happyhouse/controller/ScoreContoller.java
@@ -0,0 +1,15 @@
+package com.happyhouse.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ScoreContoller {
+	
+	//1.해당 매물의 번호와 조건을 통해, 매물 점수 매기기
+	
+	//2.해당 매물의 번호를 통해, 아파트 매물 테이블에서 해당 매물의 위치(1개)와 dong 추출
+	
+	//3.해당 매물의 dong을 통해, 공공데이터의 위치(여러개) 추출
+	
+	
+}
