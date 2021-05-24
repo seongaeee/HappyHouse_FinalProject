0c1b21d7dd802fdd57ce39f913f6e6884e763832
정물결 <jmk1473@naver.com>
2021-05-24 14:39:42 +0900

    feat : Add detail table

---
+++ happy-house-front/src/components/header/DetailHeader.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="detail-header">
+		<img alt="Qna logo" src="../../assets/house/house3.jpg" height=300px />
+		<h1>건물 정보</h1>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style>
+	#detail-header{
+	width : 100%;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+
+	}
+	h1 {
+		color: #2f3b52;
+		font-weight: 900;
+        margin-bottom: 5%;
+	}
+	img{
+		margin-top: 5%;
+        margin-bottom: 2%;
+	}
+</style>
--- happy-house-front/src/components/result/Detail.vue
+++ happy-house-front/src/components/result/Detail.vue
@@ -1,42 +1,50 @@
 <template>
   <div id="detail">
-    <result-header></result-header>
-    <section>
-      {{aptdetail.no}}
-      <b-row>
-        <b-col cols="1">
-        </b-col>
-        <b-col cols="4">
-          <div id ="detail-table">
-            <b-table striped hover>
-              <b-tr>
-                <b-th>번호</b-th>
-                <b-td>하이</b-td>
-              </b-tr>
-            </b-table>
-          </div>
-        </b-col>
-        <b-col cols="1">
-        </b-col>
-        <b-col cols="6">
-        </b-col>
-      </b-row>
+    <detail-header></detail-header>
+    <section id="field">
+      <div>
+        <b-row>
+          <b-col cols="1">
+          </b-col>
+          <b-col cols="4">
+            <div>
+              <b-table striped hover :items="items"></b-table>
+            </div>
+          </b-col>
+          <b-col cols="1"></b-col>
+          <b-col cols="6">
+          </b-col>
+        </b-row>
+      </div>
     </section>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import ResultHeader from '../header/ResultHeader.vue';
+import DetailHeader from '../header/DetailHeader.vue';
 
 export default {
-  components: { ResultHeader },
+  components: { DetailHeader },
     created() {
       var no = this.$route.params.no;
 			this.$store.dispatch("DETAIL", {no:no});
 		},
   computed: {
     ...mapGetters(["aptdetail"]),
+    items(){
+      var items = [
+          { 구분 : '번호', 정보: this.aptdetail.no},
+          { 구분 : '동', 정보: this.aptdetail.dong},
+          { 구분 : '아파트', 정보: this.aptdetail.aptName},
+          { 구분 : '코드', 정보: this.aptdetail.code},
+          { 구분 : '건축년도', 정보: this.aptdetail.buildYear},
+          { 구분 : '지번', 정보: this.aptdetail.jibun},
+          { 구분 : '위도', 정보: this.aptdetail.lat},
+          { 구분 : '경도', 정보: this.aptdetail.lng},
+        ];
+      return items;
+    }
   },
   methods: {
 
@@ -62,7 +70,7 @@ export default {
 	img{
 		margin-top: 8%;
 	}
-  #detail-table{
+  #field{
     margin-top : 2%;
     margin-bottom: 5%;
   }
