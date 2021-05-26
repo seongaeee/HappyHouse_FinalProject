7544dd435330dc6d9b23070b56af644aef1d1a81
정물결 <jmk1473@naver.com>
2021-05-26 21:27:38 +0900

    fix : map error

--- happy-house-front/src/components/result/Detail.vue
+++ happy-house-front/src/components/result/Detail.vue
@@ -1,189 +1,202 @@
 <template>
-  <div id="detail">
-    <detail-header></detail-header>
-    <section id="field">
-      <div>
-        <b-row>
-          <b-col cols="1">
-          </b-col>
-          <b-col cols="5">
-            <div>
-              <h1>매물 정보</h1>
-              <b-table striped hover :items="items"></b-table>
-            </div>
-          </b-col>
-          <b-col cols="5">
-            <h1>매물 위치</h1>
-            <div id ="map" style="width:100%;height:100%;"></div>
-          </b-col>
-          <b-col col="1">
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col col="4"></b-col>
-            <b-col col="4">
-              <div id="stars" v-if="this.aptdetail.score != null" style="margin-top:15%">
-                <h2 class="mt-3">개인 맞춤 점수</h2>
-                  <b-progress :max="10" height="2rem">
-                    <b-progress-bar :value="value" :label="`${((value / 10) * 10).toFixed(2)}점`"></b-progress-bar>
-                  </b-progress>
-              
-              </div>
-            </b-col>
-          <b-col col="4"></b-col>
-        </b-row>
-
-
-      </div>
-    </section>
-  </div>
+	<div id="detail">
+		<detail-header></detail-header>
+		<section id="field">
+			<div>
+				<b-row>
+					<b-col cols="1"> </b-col>
+					<b-col cols="5">
+						<div>
+							<h1>매물 정보</h1>
+							<b-table striped hover :items="getItems"></b-table>
+						</div>
+					</b-col>
+					<b-col cols="5">
+						<h1>매물 위치</h1>
+						<div id="map" style="width: 100%; height: 100%"></div>
+					</b-col>
+					<b-col col="1"> </b-col>
+				</b-row>
+				<b-row>
+					<b-col col="4"></b-col>
+					<b-col col="4">
+						<div
+							id="stars"
+							v-if="this.aptdetail.score != null"
+							style="margin-top: 15%"
+						>
+							<h2 class="mt-3">개인 맞춤 점수</h2>
+							<b-progress :max="10" height="2rem">
+								<b-progress-bar
+									:value="value"
+									:label="`${((value / 10) * 10).toFixed(2)}점`"
+								></b-progress-bar>
+							</b-progress>
+						</div>
+					</b-col>
+					<b-col col="4"></b-col>
+				</b-row>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import DetailHeader from '../header/DetailHeader.vue';
+	//	import { mapGetters } from "vuex";
+	import DetailHeader from "../header/DetailHeader.vue";
 
-export default {
-  components: { DetailHeader },
-    created() {
-      this.dong = this.$route.params.dong;
-      this.aptName = this.$route.params.aptName;
-      this.id = localStorage.id;
-      this.car = localStorage.car;
-      this.pet = localStorage.pet;
-      this.scoreCheck = localStorage.scoreCheck;
-      this.office_address_base =  localStorage.office_address_base;
-      this.office_zip = localStorage.office_zip;
+	export default {
+		components: { DetailHeader },
+		created() {
+			this.dong = this.$route.params.dong;
+			this.aptName = this.$route.params.aptName;
+			this.id = localStorage.id;
+			this.car = localStorage.car;
+			this.pet = localStorage.pet;
+			this.scoreCheck = localStorage.scoreCheck;
+			this.office_address_base = localStorage.office_address_base;
+			this.office_zip = localStorage.office_zip;
 
-      if(this.id != null){
-        this.$store.dispatch("UPGRADEDETAIL", {
-          dong : this.dong, 
-          aptName : this.aptName,
-          id : this.id,
-          car : this.car,
-          pet : this.pet,
-          scoreCheck : this.scoreCheck,
-          office_address_base : this.office_address_base,
-          office_zip : this.office_zip,
-        }).then(()=>{
-          this.mapCheck();
-        });
-      }
-      else{
-        this.$store.dispatch("DETAIL", {
-          dong : this.dong, 
-          aptName : this.aptName,
-        }).then(()=>{
-          this.mapCheck();
-        });
-      }
-        },
-    computed: {
-    ...mapGetters(["aptdetail"]),
-    items(){
-      var items1 = [
-          { 구분 : '번호', 정보: this.saptdetail.no},
-          { 구분 : '동', 정보: this.aptdetail.dong},
-          { 구분 : '아파트', 정보: this.aptdetail.aptName},
-          { 구분 : '코드', 정보: this.aptdetail.code},
-          { 구분 : '건축년도', 정보: this.aptdetail.buildYear},
-          { 구분 : '지번', 정보: this.aptdetail.jibun},
-          { 구분 : '위도', 정보: this.aptdetail.lat},
-          { 구분 : '경도', 정보: this.aptdetail.lng},
-        ];
+			if (this.id != null) {
+				this.$store
+					.dispatch("UPGRADEDETAIL", {
+						dong: this.dong,
+						aptName: this.aptName,
+						id: this.id,
+						car: this.car,
+						pet: this.pet,
+						scoreCheck: this.scoreCheck,
+						office_address_base: this.office_address_base,
+						office_zip: this.office_zip,
+					})
+					.then(() => {
+						//this.mapCheck();
+						this.aptdetail = this.$store.state.aptdetail;
+            			this.value = this.$store.state.aptdetail.score;
+						this.mapCheck();
+					});
+			} else {
+				//일단 로그인 안한 경우만 처리
 
-      var items2 = [
-          { 구분 : '번호', 정보: this.aptdetail.no},
-          { 구분 : '동', 정보: this.aptdetail.dong},
-          { 구분 : '아파트', 정보: this.aptdetail.aptName},
-          { 구분 : '코드', 정보: this.aptdetail.code},
-          { 구분 : '건축년도', 정보: this.aptdetail.buildYear},
-          { 구분 : '지번', 정보: this.aptdetail.jibun},
-          { 구분 : '위도', 정보: this.aptdetail.lat},
-          { 구분 : '경도', 정보: this.aptdetail.lng},
-          { 구분 : '점수', 정보: this.aptdetail.score},
-        ];
+				this.$store
+					.dispatch("DETAIL", {
+						dong: this.dong,
+						aptName: this.aptName,
+					})
+					.then(() => {
+						this.aptdetail = this.$store.state.aptdetail;
+						this.mapCheck();
+					});
+			}
+		},
+		mounted() {
+			//this.mapCheck();
+		},
+		methods: {
+			getItems() {
+				var items1 = [
+					{ 구분: "번호", 정보: this.$store.state.aptdetail.no },
+					{ 구분: "동", 정보: this.aptdetail.dong },
+					{ 구분: "아파트", 정보: this.aptdetail.aptName },
+					{ 구분: "코드", 정보: this.aptdetail.code },
+					{ 구분: "건축년도", 정보: this.aptdetail.buildYear },
+					{ 구분: "지번", 정보: this.aptdetail.jibun },
+					{ 구분: "위도", 정보: this.aptdetail.lat },
+					{ 구분: "경도", 정보: this.aptdetail.lng },
+				];
 
-      if(this.aptdetail.score == null){
-        return items1;
-      }else{
-        return items2;
-      }
-    },
-    value(){
-      return this.aptdetail.score;
-    }
-  },
-  mounted() {
-    this.mapCheck();
-  },
-  methods: {
-    mapCheck(){
-      if (window.kakao && window.kakao.maps) {
-        this.initMap();
-      } else {
-        const script = document.createElement('script');
-        /* global kakao */
-        script.onload = () => kakao.maps.load(this.initMap);
-        script.src =
-          'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=242fb4c309b28366b554b85bdd482fc5';
-        document.head.appendChild(script);
-      }
-    },
-    initMap() {
-      var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-          mapOption = {
-            center: new kakao.maps.LatLng(this.aptdetail.lat, this.aptdetail.lng), // 지도의 중심좌표
-            level: 3, // 지도의 확대 레벨
-          };
+				var items2 = [
+					{ 구분: "번호", 정보: this.aptdetail.no },
+					{ 구분: "동", 정보: this.aptdetail.dong },
+					{ 구분: "아파트", 정보: this.aptdetail.aptName },
+					{ 구분: "코드", 정보: this.aptdetail.code },
+					{ 구분: "건축년도", 정보: this.aptdetail.buildYear },
+					{ 구분: "지번", 정보: this.aptdetail.jibun },
+					{ 구분: "위도", 정보: this.aptdetail.lat },
+					{ 구분: "경도", 정보: this.aptdetail.lng },
+					{ 구분: "점수", 정보: this.aptdetail.score },
+				];
 
-            var map2 = new kakao.maps.Map(mapContainer, mapOption);
+				if (this.aptdetail.score == null) {
+					return items1;
+				} else {
+					return items2;
+				}
+			},
+			mapCheck() {
+				if (window.kakao && window.kakao.maps) {
+					alert("second");
+					this.initMap();
+				} else {
+					alert("first");
+					const script = document.createElement("script");
+					/* global kakao */
+					script.onload = () => kakao.maps.load(this.initMap);
+					script.src =
+						"http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=242fb4c309b28366b554b85bdd482fc5";
+					document.head.appendChild(script);
+				}
+			},
+			initMap() {
+				var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+					mapOption = {
+						center: new kakao.maps.LatLng(
+							this.aptdetail.lat,
+							this.aptdetail.lng
+						), // 지도의 중심좌표
+						level: 3, // 지도의 확대 레벨
+					};
 
-            var markerPosition  = new kakao.maps.LatLng(this.aptdetail.lat, this.aptdetail.lng); 
-            var marker = new kakao.maps.Marker({
-                 position: markerPosition
-            });
-            marker.setMap(map2);
-          console.log(map2);
-    }
-  },
-  data() {
-    return {
-      max : 10,
-      dong : "",
-      aptName : "",
-      id : "",
-      car : "",
-      pet : "",
-      scoreCheck : "",
-      office_address_base : "",
-      office_zip : "",
-    }
-  },
-}
+				var map2 = new kakao.maps.Map(mapContainer, mapOption);
+
+				var markerPosition = new kakao.maps.LatLng(
+					this.aptdetail.lat,
+					this.aptdetail.lng
+				);
+				var marker = new kakao.maps.Marker({
+					position: markerPosition,
+				});
+				marker.setMap(map2);
+				console.log(map2);
+			},
+		},
+		data() {
+			return {
+				aptdetail: null,
+				max: 10,
+				dong: "",
+				aptName: "",
+				id: "",
+				car: "",
+				pet: "",
+				scoreCheck: "",
+				office_address_base: "",
+				office_zip: "",
+        		value: "",
+			};
+		},
+	};
 </script>
 
 <style>
-#detail{
-    width : 100%;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-
-    }
-    h1 {
-        color: #2f3b52;
-        font-weight: 900;
-        margin: 2.5rem 0 1.5rem;
-    }
-    img{
-        margin-top: 8%;
-    }
-  #field{
-    margin-top : 2%;
-    margin-bottom: 5%;
-  }
-
+	#detail {
+		width: 100%;
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+	h1 {
+		color: #2f3b52;
+		font-weight: 900;
+		margin: 2.5rem 0 1.5rem;
+	}
+	img {
+		margin-top: 8%;
+	}
+	#field {
+		margin-top: 2%;
+		margin-bottom: 5%;
+	}
 </style>
--- happy-house-front/src/components/result/Result.vue
+++ happy-house-front/src/components/result/Result.vue
@@ -1,109 +1,123 @@
 <template>
-  <div id="apt-list">
-    <result-header></result-header>
-    <section>
-      <b-row>
-        <b-col></b-col>
-              <b-col cols="8">
-                  <div id="result-table">
-                      <b-table
-                      striped
-                      hover
-                      :items="aptlist"
-                      :per-page="perPage"
-                      :current-page="currentPage"
-                      :fields="fields"
-                      @row-clicked="rowClick"
-                      ></b-table>
-                      <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage" align="center" style="margin-top:2%; margin-bottom:2%;"></b-pagination>
-                  </div>
-              </b-col>
-          <b-col></b-col>
-      </b-row>
-    </section>
-  </div>
+	<div id="apt-list">
+		<result-header></result-header>
+		<section>
+			<b-row>
+				<b-col></b-col>
+				<b-col cols="8">
+					<div id="result-table">
+						<b-table
+							striped
+							hover
+							:items="aptlist"
+							:per-page="perPage"
+							:current-page="currentPage"
+							:fields="fields"
+							@row-clicked="rowClick"
+						></b-table>
+						<b-pagination
+							v-model="currentPage"
+							:total-rows="rows"
+							:per-page="perPage"
+							align="center"
+							style="margin-top: 2%; margin-bottom: 2%"
+						></b-pagination>
+					</div>
+				</b-col>
+				<b-col></b-col>
+			</b-row>
+		</section>
+	</div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import ResultHeader from '../header/ResultHeader.vue';
+	import { mapGetters } from "vuex";
+	import ResultHeader from "../header/ResultHeader.vue";
 
-export default {
-  components: { ResultHeader },
-    created() {
-      var dong = this.$route.params.dong;
-			this.$store.dispatch("SEARCH", {dong:dong});
+	export default {
+		components: { ResultHeader },
+		created() {
+			var dong = this.$route.params.dong;
+			//this.$route.params.dong;
+			this.$store.dispatch("SEARCH", { dong: dong });
 		},
-    data() {
-    return {
-      currentPage: 1, // 현재 페이지
-      perPage: 5, // 페이지당 보여줄 갯수
-      // bootstrap 'b-table' 필드 설정
-      fields: [
-        {
-          key: "no",
-          label: "번호"
-        },
-        {
-          key: "dong",
-          label: "동"
-        },
-        {
-          key: "aptName",
-          label: "아파트"
-        },
-        {
-          key: "code",
-          label: "코드"
-        },
-        {
-          key: "dealAmount",
-          label: "거래금액"
-        },
-        {
-          key: "buildYear",
-          label: "거래년도"
-        },
-        {
-          key: "dealYear",
-          label: "거래년도"
-        },
-        {
-          key: "dealMonth",
-          label: "거래월"
-        },
-        {
-          key: "dealDay",
-          label: "거래일"
-        },
-        {
-          key: "area",
-          label: "면적"
-        },
-        {
-          key: "floor",
-          label: "층"
-        },
-      ],
-    };
-  },
-  computed: {
-    ...mapGetters(["aptlist"]),
-    rows() {
-      return this.aptlist.length;
-    },
-  },
-  methods: {
-    rowClick(item) {
-      //첫 호출시 에러가 날 수 있어 미리 채워둔다.
-      this.$store.dispatch("DETAIL", {dong:item.dong, aptName:item.aptName});
-      this.$router.push({
-        path: `/detail/${item.dong}/${item.aptName}`
-      });
-      
-    },
-  },
-}
+		data() {
+			return {
+				currentPage: 1, // 현재 페이지
+				perPage: 5, // 페이지당 보여줄 갯수
+				// bootstrap 'b-table' 필드 설정
+				fields: [
+					{
+						key: "no",
+						label: "번호",
+					},
+					{
+						key: "dong",
+						label: "동",
+					},
+					{
+						key: "aptName",
+						label: "아파트",
+					},
+					{
+						key: "code",
+						label: "코드",
+					},
+					{
+						key: "dealAmount",
+						label: "거래금액",
+					},
+					{
+						key: "buildYear",
+						label: "거래년도",
+					},
+					{
+						key: "dealYear",
+						label: "거래년도",
+					},
+					{
+						key: "dealMonth",
+						label: "거래월",
+					},
+					{
+						key: "dealDay",
+						label: "거래일",
+					},
+					{
+						key: "area",
+						label: "면적",
+					},
+					{
+						key: "floor",
+						label: "층",
+					},
+				],
+			};
+		},
+		computed: {
+			...mapGetters(["aptlist"]),
+			rows() {
+				return this.aptlist.length;
+			},
+		},
+		methods: {
+			rowClick(item) {
+				this.$router.push({
+					path: `/detail/${item.dong}/${item.aptName}`,
+				});
+				//첫 호출시 에러가 날 수 있어 미리 채워둔다.
+				/*
+				this.$store
+					.dispatch("DETAIL", { dong: item.dong, aptName: item.aptName })
+					.then(() => {
+						console.log("ㅠㅠ:", this.$store.state.aptdetail);
+						this.$router.push({
+							path: `/detail/${item.dong}/${item.aptName}`,
+						});
+					}); */
+			},
+		},
+	};
 </script>
 
 <style>
@@ -112,12 +126,11 @@ export default {
 		font-weight: 900;
 		margin: 2.5rem 0 1.5rem;
 	}
-	img{
+	img {
 		margin-top: 8%;
 	}
-  #result-table{
-    margin-top : 3%;
-    margin-bottom : 5%;
-  }
-
+	#result-table {
+		margin-top: 3%;
+		margin-bottom: 5%;
+	}
 </style>
