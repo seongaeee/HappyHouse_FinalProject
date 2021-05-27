8e76a33e52f11b02230d0ae3a97154fabdeb39d7
정물결 <jmk1473@naver.com>
2021-05-27 14:27:23 +0900

    fix : Footer  내용 수정

--- happy-house-front/src/components/footer/BasicFooter.vue
+++ happy-house-front/src/components/footer/BasicFooter.vue
@@ -23,25 +23,24 @@
     <div class="footer-center">
       <div>
         <i class="fa fa-map-marker"></i>
-        <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
+        <p><span>107, Hanamsandan 6beon-ro, Gwangsan-gu,</span> Gwangju, Republic of Korea</p>
       </div>
 
       <div>
         <i class="fa fa-phone"></i>
-        <p>+1.555.555.5555</p>
+        <p>+82.062.324.5365</p>
       </div>
 
       <div>
         <i class="fa fa-envelope"></i>
-        <p><a href="mailto:support@company.com">support@company.com</a></p>
+        <p><a href="mailto:support@company.com">ssafy@ssafy.com</a></p>
       </div>
     </div>
 
     <div class="footer-right">
       <p class="footer-company-about">
-        <span>About the company</span>
-        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu
-        auctor lacus vehicula sit amet.
+        <span>HAPPYHOUSE</span>
+        부동산 매물에 개인 맞춤형 점수를 제공해주는 웹 서비스
       </p>
 
       <div class="footer-icons">
